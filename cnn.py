28a2c249504c405e4de6e0c8803192ce2e50c763
Lab User <labuser@d25xp3kwj1gq.infosolco.net>
2019-12-20 16:31:34 -0800

    cnn

--- cnn.py
+++ cnn.py
@@ -1,9 +1,25 @@
 import numpy as np
 
+from common.batch_engine import BatchEngine
+from layers.cross_entropy import CrossEntropy
+
 
 class Cnn:
+
+    loss_function = CrossEntropy()
+
     def __init__(self):
         self.layers = []
 
     def add_layer(self, layer):
         self.layers.append(layer)
+
+    def learn(self, images, labels):
+        batch_engine = BatchEngine(self.layers, self.loss_function)
+        batch_engine.run(images, labels)
+
+    def predict(self, image):
+        for layer in self.layers:
+            image = layer.forward(image)
+
+        return np.argmax(image)
