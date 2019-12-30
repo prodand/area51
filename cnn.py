4721e5ac742d786d82d9659548cfe3c03a2e4fd5
Lab User <labuser@d25xp3kwj1gq.infosolco.net>
2019-12-30 09:55:58 -0800

    Batch process only one 1 epoch, moved spliting train/val data into cnn class

--- cnn.py
+++ cnn.py
@@ -18,6 +18,7 @@ class Cnn:
     def __init__(self, batch_size):
         self.layers = []
         self.batch_size = batch_size
+        self.folds_number = 10
 
     def add_layer(self, layer):
         self.layers.append(layer)
@@ -29,7 +30,22 @@ class Cnn:
         :return:
         """
         batch_engine = BatchEngine(self.layers, self.loss_function, 0.1, self.batch_size)
-        batch_engine.run(images, labels)
+
+        learned = False
+        epoch = 1
+        excluded_fold = 0
+        while not learned:
+            train_images, train_labels = self.extract_train_data(images, labels, excluded_fold)
+            train_loss = batch_engine.run(train_images, train_labels, epoch)
+
+            validation_images, validation_labels = self.extract_validate_data(images, labels, excluded_fold)
+            validation_loss, percent = batch_engine.validate(validation_images, validation_labels)
+
+            print("Val: %s; Train: %s; Epoch: %s; Percent: %s" % (validation_loss, train_loss, epoch, percent))
+
+            excluded_fold = excluded_fold + 1 if excluded_fold < (self.folds_number - 1) else 0
+            epoch += 1
+            learned = train_loss < 0.01
         self.save()
 
     def predict(self, image):
@@ -37,6 +53,23 @@ class Cnn:
             image = layer.forward(image)
 
         return np.argmax(image)
+
+    def extract_train_data(self, images, labels, excluded_fold):
+        fold_size = int(len(images) / self.folds_number)
+        fold_start = excluded_fold * fold_size
+        fold_end = (excluded_fold + 1) * fold_size
+        if excluded_fold == 0:
+            return images[fold_end:], labels[fold_end:]
+        if excluded_fold == (self.folds_number - 1):
+            return images[0:fold_start], labels[0:fold_start]
+        return np.concatenate((images[0:fold_start], images[fold_end:])), np.concatenate(
+            (labels[0:fold_start], labels[fold_end:]))
+
+    def extract_validate_data(self, images, labels, fold: int):
+        fold_size = int(len(images) / self.folds_number)
+        fold_start = fold * fold_size
+        fold_end = (fold + 1) * fold_size
+        return images[fold_start:fold_end], labels[fold_start:fold_end]
 
     def save(self):
         print("Here I should be saved")
--- plain/common/batch_engine.py
+++ plain/common/batch_engine.py
@@ -12,39 +12,22 @@ class BatchEngine:
         self.batch_size = batch_size
         self.learning_rate = learning_rate
         self.cache = list()
-        self.folds_number = 10
         self.train_plt_loss = []
         self.validation_plt_loss = []
 
-    def run(self, images, labels):
-        learned = False
-        epoch = 1
-        excluded_fold = 0
-        while not learned:
-            print('New Round')
-            train_loss = 0
-            for fold in range(0, self.folds_number):
-                if fold == excluded_fold:
-                    continue
-                train_images = self.extract_fold(images, fold)
-                train_labels = self.extract_fold(labels, fold)
-                batches_count = int(len(train_images) / self.batch_size)
-                for batch_index in range(0, batches_count):
-                    start = batch_index * self.batch_size
-                    end = (batch_index + 1) * self.batch_size
-                    start_time = time.time()
-                    loss = self.run_batch(train_images[start:end], train_labels[start:end], epoch)
-                    train_loss += loss
-                    print('%s Loss: %s [%s]' % (epoch, str(loss / self.batch_size), (time.time() - start_time)))
+    def run(self, images, labels, epoch):
+        print('New Round')
+        train_loss = 0
+        batches_count = int(len(images) / self.batch_size)
+        for batch_index in range(0, batches_count):
+            start = batch_index * self.batch_size
+            end = (batch_index + 1) * self.batch_size
+            start_time = time.time()
+            loss = self.run_batch(images[start:end], labels[start:end], epoch)
+            train_loss += loss
+            print('%s Loss: %s [%s]' % (epoch, str(loss / self.batch_size), (time.time() - start_time)))
 
-            train_loss = self.average_train_loss(images, train_loss)
-            validation_loss, percent = self.validate(self.extract_fold(images, excluded_fold),
-                                            self.extract_fold(labels, excluded_fold))
-            # self.plot(train_loss, validation_loss)
-            print("Val: %s; Train: %s; Epoch: %s; Percent: %s" % (validation_loss, train_loss, epoch, percent))
-            excluded_fold = excluded_fold + 1 if excluded_fold < self.folds_number else 0
-            epoch += 1
-            learned = train_loss < 0.01
+        return train_loss / len(images)
 
     def validate(self, images, labels):
         total_loss = 0
@@ -53,14 +36,14 @@ class BatchEngine:
             convolved_image = image
             for layer in self.layers:
                 convolved_image = layer.forward(convolved_image)
-            actual = np.argmax(image)
+            actual = np.argmax(convolved_image)
             correct_answers += labels[index][actual]
             error = self.loss_fn.loss(convolved_image, labels[index])
             total_loss += error
 
         return total_loss / len(images), correct_answers / len(images)
 
-    def run_batch(self, images, labels, epoch = 1):
+    def run_batch(self, images, labels, epoch=1):
         total_loss = 0
         layers_cache = list()
         for layer in self.layers:
@@ -93,16 +76,6 @@ class BatchEngine:
             layer.update_weights(cache, self.learning_rate / epoch)
 
         return total_loss
-
-    def extract_fold(self, images, fold: int):
-        fold_size = int(len(images) / self.folds_number)
-        fold_start = fold * fold_size
-        fold_end = (fold + 1) * fold_size
-        return images[fold_start:fold_end]
-
-    def average_train_loss(self, images, loss):
-        images_count = len(images) * (self.folds_number - 1) / self.folds_number
-        return loss / images_count
 
     def plot(self, train_loss, validation_loss):
         self.train_plt_loss.append(train_loss)
